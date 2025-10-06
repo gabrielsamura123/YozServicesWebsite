804218a791a15ab0e7cd784cfb1fa9d00955b4b8
gabrielsamura123 <gabrielsamura9@gmail.com>
2025-10-06 13:19:02 +0000

    Added my gallery data for fetching and displaying images on the website

---
+++ galleryData.js
@@ -0,0 +1,30 @@
+export const galleryData = [
+    {
+        name: "Dell Visit",
+        desc: "We met for their servers and products devlivered at Yoz Services Limited Sierra Leone",
+        fullDesc: "A productive meet-up with Dell company for the distributions of their products which includes: servers, laptop and systems which was delivered at Yoz Services Limited Sierra Leone. For the best IT solutions, think Yoz Services Limited. We are committed to delivering top-notch IT solutions and services to our clients. Our partnership with Dell allows us to provide our clients with the latest technology and products to help them achieve their business goals.",
+        img: "/src/assets/Mr. Charles & Dell.jpg",
+        id: 1,
+    },
+    {
+          name: "World Bank Visit",
+          desc: "We met with the World Bank team for a project discussion and collaboration. At World Bank.",
+          fullDesc: "A productive meet-up with the World Bank team for a project discussion and collaboration. Yoz Services Limited is committed to delivering top-notch IT solutions and services to our clients. We are proud to partner with the World Bank to support their mission of reducing poverty and promoting sustainable development around the world.",
+          img: "/src/assets/world_bank_meet_up.jpg",
+          id: 2,
+    },
+    {
+          name: "Cisco Visit",
+          desc: "We met with the Cisco team for a project discussion and collaboration. At Radison Blue Hotel",
+          fullDesc: "CISCO donates Video Conferencing its to GoSL, Statehouse, Freetown, Sierra Leone, 11th May, 2020. The Honourable Chief Minister, Professor David John Franncis has received 4 Video conferencing kits from CISCO as a donation to the Government of Sierra Leone. The donation was made possible through the efforts of Yoz Services Limited, the local partner for CISCO in Sierra Leone. The Honourable Chief Minister thanked CISCO and Yoz Services Limited for the kind gesture and assured them that the equipment will be put to good use by the Government of Sierra Leone.",
+          img: "/src/assets/cisco_meet_up.jpg",
+          id: 3,
+    },
+    {
+          name: "England High Commission Visit",
+          desc: "We met with the England High Commission team for a project discussion and collaboration. At England with their footbal player",
+          fullDesc: "A productive meet-up with the England High Commission team for a project discussion and collaboration. Yoz Services Limited is committed to delivering top-notch IT solutions and services to our clients. We are proud to partner with the England High Commission to support their mission of promoting democracy, human rights, and sustainable development around the world.",
+          img: "/src/assets/Yoz_Football_Player.jpg",
+          id: 4,
+    }
+];
